6b4719e0c3320d1d1f5d5d68c321902804b999cb
Harsha <harshamalhotra.ubs@gmail.com>
2017-04-11 17:30:52 -0700

    Checkpoint 23 half completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var animatePoints=function(){
-    var points = document.getElementsByClassName("point");
+var pointsArray=document.getElementsByClassName("point");
 
-    var revealPoints=function(i){
+var animatePoints=function(points){
+        var revealPoint=function(i){
         points[i].style.opacity=1;
         points[i].style.transform="scaleX(1) translateY(0)";
         points[i].style.msTransform="scaleX(1) translateY(0)";
@@ -10,10 +10,24 @@ var animatePoints=function(){
     };
 
     for(var x=0;x<points.length;x++){
-        revealPoints(x);
+        revealPoint(x);
     }
 };
-   
+
+window.onload=function(){
+    if(window.innerHeight>950){
+        animatePoints(pointsArray);
+    }
+    var sellingPoints=document.getElementsByClassName("selling-points")[0];
+    var scrollDistance=sellingPoints.getBoundingClientRect().top-window.innerHeight +200;
+
+    window.addEventListener('scroll',function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+            animatePoints(pointsArray);    
+        }   
+    }
+    );
+};      
 
         
         
